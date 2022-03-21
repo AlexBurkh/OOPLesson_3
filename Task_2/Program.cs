9f3159a99e2c6144a7524fe8f435d15d6841460c
AlexBurkh <alex.burkh@yandex.ru>
2022-03-21 21:41:21 +0300

    Выполнена задача 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firststring = "Hello";
+            string secondstring = ReverseString(firststring);
+        }
+        static string ReverseString(string input)
+        {
+            string temp = string.Empty;
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                temp += input[i];
+            }
+            return temp;
+        }
+    }
+}
