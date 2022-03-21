1ad598c5e482d4b558a7a8961e93f378cad2e63a
AlexBurkh <alex.burkh@yandex.ru>
2022-03-21 22:08:35 +0300

    Выполнена задача 3(*)

---
+++ Task_3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string srcPath = "src.txt";
+            string outputPath = "output.txt";
+            string[] lines = LoadSrcFileLines(srcPath);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                SearchMail(ref lines[i]);
+            }
+            File.WriteAllLines(outputPath, lines);
+        }
+        static string[] LoadSrcFileLines(string path)
+        {
+            return File.ReadAllLines(path);
+        }
+        static void SearchMail(ref string src)
+        {
+            bool flag = false;
+            string temp = string.Empty;
+            for (int i = 0; i < src.Length; i++)
+            {
+                if ((flag == true) && (src[i] != ' '))
+                {
+                    temp += src[i];
+                    continue;
+                }
+                if (src[i] == '&')
+                {
+                    flag = true;
+                }
+            }
+            src = temp;
+        }
+    }
+}
